dc03faf11f8191782e6de3c92699ea4878086c25
Your Name <you@example.com>
2020-08-02 22:04:27 +0530

    Updates

---
+++ precache-manifest.50e166a4ea27fa61692eee34043f0bde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc75eebd1006e8024270c420f6fd1dc8",
+    "url": "/react-projects/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/react-projects/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b5f8aed291d4876cba23",
+    "url": "/react-projects/static/css/main.4bbedf89.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/react-projects/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-projects/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f8aed291d4876cba23",
+    "url": "/react-projects/static/js/main.94620118.chunk.js"
+  },
+  {
+    "revision": "46d30b5ceef2a7725071",
+    "url": "/react-projects/static/js/runtime-main.da15128f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-projects/static/media/SEGA.12a3db8b.woff"
+  }
+]);
